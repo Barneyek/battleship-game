5799d47807e3159b96ab534c8e9711d26ae17554
Damian Osiecki <osieckidamian1992@gmail.com>
2019-10-14 23:22:21 +0200

    Add object model with parameters

--- battleships.js
+++ battleships.js
@@ -20,16 +20,45 @@ var model = {
     shipLength: 3,
     shipsSunk: 0,
     ships: [{
-            locations: [0, 0, 0],
-            hits: ["", "", ""]
-        },
-        {
-            locations: [0, 0, 0],
-            hits: ["", "", ""]
-        },
-        {
-            locations: [0, 0, 0],
-            hits: ["", "", ""]
-        }
+        locations: [0, 0, 0],
+        hits: ["", "", ""]
+    },
+    {
+        locations: [0, 0, 0],
+        hits: ["", "", ""]
+    },
+    {
+        locations: [0, 0, 0],
+        hits: ["", "", ""]
+    }
     ],
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("TRAFIONY!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("Zatopiłeś mój okręt!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("Pudło!");
+        return false;
+    },
+
+    isSunk: function (ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] != "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
 };
+
