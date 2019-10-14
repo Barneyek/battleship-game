778715194860350e5a719c07a69e8df9754402b0
Damian Osiecki <osieckidamian1992@gmail.com>
2019-10-14 22:32:53 +0200

    Add object model with parameters

--- battleships.js
+++ battleships.js
@@ -14,3 +14,22 @@ var view = {
         cell.setAttribute("class", "miss");
     }
 };
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [{
+            locations: [0, 0, 0],
+            hits: ["", "", ""]
+        },
+        {
+            locations: [0, 0, 0],
+            hits: ["", "", ""]
+        },
+        {
+            locations: [0, 0, 0],
+            hits: ["", "", ""]
+        }
+    ],
+};
