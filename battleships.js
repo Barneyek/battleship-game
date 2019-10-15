08df5ea5a816e938cb273fc43cd800ebd9c2787f
Damian Osiecki <osieckidamian1992@gmail.com>
2019-10-15 17:59:21 +0200

    Add collision function

--- battleships.js
+++ battleships.js
@@ -62,6 +62,15 @@ var model = {
         return newShipLocations;
     },
     collision: function (locations) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = model.ships[i];
+            for (var j = 0; j < locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     },
     fire: function (guess) {
         for (var i = 0; i < this.numShips; i++) {
